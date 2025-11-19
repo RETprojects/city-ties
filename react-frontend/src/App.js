2e346f1c2bab8e795bad51d5c86f8b88fca76079
remi <remitoutin@gmail.com>
2025-11-19 01:44:50 +0100

    Getting data from backend to frontend (beginnings)

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,25 +1,52 @@
+// thanks to Dipal Bhavsar: https://www.bacancytechnology.com/blog/react-with-python
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const App = () => {
+    const [data, setData] = useState('');
+
+    // get the data from the backend & use it
+    useEffect(() => {
+        axios.get('http://127.0.0.1:5000/api/data') // Flask
+            .then(response => {
+                setData(response.data.message);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h1>React with Python</h1>
+            <p>{data}</p>
+        </div>
+    );
+};
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
