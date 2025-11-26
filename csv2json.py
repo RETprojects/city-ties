d1cb21a05b4b7699646aaa0650e46c5f7a10010d
remi <remitoutin@gmail.com>
2025-11-26 17:39:50 +0100

    Got JSON Lines file for national capitals

--- csv2json.py
+++ csv2json.py
@@ -2,10 +2,8 @@
 # thanks to: https://www.geeksforgeeks.org/python/convert-csv-to-json-using-python/
 
 import csv
-import json
+import jsonlines
 
-with open('national_capitals_no_duplicates.csv', mode='r', newline='', encoding='utf-8') as csvfile:
-    data = list(csv.DictReader(csvfile))
-
-with open('national_capitals.json', mode='w', encoding='utf-8') as jsonfile:
-    json.dump(data, jsonfile, indent=4)
+with open('national_capitals_no_duplicates.csv', mode='r', newline='', encoding='utf-8') as csvfile, jsonlines.open('national_capitals.jsonl', mode='w') as writer:
+    for row in csv.DictReader(csvfile):
+        writer.write(row)
